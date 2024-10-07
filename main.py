200d309aedbf7eeeb00a39582224214233cb5dac
Shuveb Hussain <shuveb@gmail.com>
2024-10-08 03:20:22 +0530

    Adding food receipt and also comparison with LP

--- main.py
+++ main.py
@@ -6,6 +6,8 @@ from pydantic import BaseModel, Field
 from langchain.prompts import SystemMessagePromptTemplate, HumanMessagePromptTemplate, ChatPromptTemplate
 from langchain_openai import ChatOpenAI
 from langchain.output_parsers import PydanticOutputParser
+from llama_parse import LlamaParse
+from llama_index.core import SimpleDirectoryReader
 
 
 # define a Pydantic schema for credit card spends with purchase date,
@@ -40,6 +42,17 @@ class GeographicFinancialStatement(BaseModel):
                                                                                "Asia Pacific region, sorted "
                                                                                "chronologically")
 
+class ReceiptLineItem(BaseModel):
+    item_name: str = Field(description="Name of the item")
+    item_quantity: int = Field(description="Quantity of the item")
+    item_total: float = Field(description="Total cost of the item")
+
+class Receipt(BaseModel):
+    vendor_name: str = Field(description="Name of the vendor")
+    purchase_date: datetime = Field(description="Date of purchase")
+    receipt_number: str = Field(description="Receipt number")
+    line_items: list[ReceiptLineItem] = Field(description="List of line items in the receipt")
+    total_amount: float = Field(description="Total amount of the receipt")
 
 def error_exit(error_message):
     print(error_message)
@@ -55,6 +68,22 @@ def extract_text_from_pdf(file_path, pages_list=None):
     except LLMWhispererClientException as e:
         error_exit(e)
 
+
+def extract_text_from_pdf_with_llamaparse(file_path, pages_list=None):
+    # set up parser
+    parser = LlamaParse(
+        result_type="markdown",  # "markdown" and "text" are available
+        target_pages=pages_list
+    )
+
+    # use SimpleDirectoryReader to parse our file
+    file_extractor = {".pdf": parser}
+    documents = SimpleDirectoryReader(input_files=[file_path], file_extractor=file_extractor).load_data()
+    extracted_text = ''
+    for doc in documents:
+        extracted_text += doc.text
+
+    return extracted_text
 
 def compile_template_and_get_llm_response(preamble, extracted_text, pydantic_object):
     postamble = "Do not include any explanation in the reply. Only include the extracted information in the reply."
@@ -81,8 +110,13 @@ def extract_cc_spend_from_text(extracted_text):
     return compile_template_and_get_llm_response(preamble, extracted_text, CreditCardSpendItems)
 
 
-def process_cc_statement():
-    extracted_text = extract_text_from_pdf("assets/docs/Chase Freedom.pdf", pages_list="3")
+def process_cc_statement(use_llamaparse=False):
+    if use_llamaparse:
+        # zero index based
+        extracted_text = extract_text_from_pdf_with_llamaparse("assets/docs/Chase Freedom.pdf", pages_list="2")
+    else:
+        # actual human index based
+        extracted_text = extract_text_from_pdf("assets/docs/Chase Freedom.pdf", pages_list="3")
     print(extracted_text)
     response = extract_cc_spend_from_text(extracted_text)
     print(response)
@@ -94,18 +128,40 @@ def extract_financial_statement_from_text(extracted_text):
     return compile_template_and_get_llm_response(preamble, extracted_text, GeographicFinancialStatement)
 
 
-def process_financial_statement():
-    extracted_text = extract_text_from_pdf("assets/docs/Apple_10-Q-Q2-2024.pdf", pages_list="14")
+def process_financial_statement(use_llamaparse=False):
+    if use_llamaparse:
+        extracted_text = extract_text_from_pdf_with_llamaparse("assets/docs/Apple_10-Q-Q2-2024.pdf", pages_list="14")
+    else:
+        extracted_text = extract_text_from_pdf("assets/docs/Apple_10-Q-Q2-2024.pdf", pages_list="14")
     print(extracted_text)
     response = extract_financial_statement_from_text(extracted_text)
     print(response)
 
+def extract_receipt_details_from_text(extracted_text):
+    preamble = ("You're seeing details of a receipt and your job is to accurately extract the "
+                "details like name of the vendor, date, total amount, and the list of items purchased, etc as instructed")
+    return compile_template_and_get_llm_response(preamble, extracted_text, Receipt)
+
+
+def process_receipt(use_llamaparse=False):
+    if use_llamaparse:
+        extracted_text = extract_text_from_pdf_with_llamaparse("assets/docs/food_receipt_phone.pdf")
+    else:
+        extracted_text = extract_text_from_pdf("assets/docs/food_receipt_phone.pdf")
+    print(extracted_text)
+    response = extract_receipt_details_from_text(extracted_text)
+    print(response)
 
 def main():
     load_dotenv()
-    process_cc_statement()
-    process_financial_statement()
-
+    if len(sys.argv) > 1 and sys.argv[1] == "llamaparse":
+        process_cc_statement(use_llamaparse=True)
+        process_financial_statement(use_llamaparse=True)
+        process_receipt(use_llamaparse=True)
+    else:
+        process_cc_statement()
+        process_financial_statement()
+        process_receipt()
 
 if __name__ == "__main__":
     main()
